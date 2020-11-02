a5a2b55960076d3d80a3addbbf588484f06ff52c
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-02 13:53:13 +0100

    1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Read instructions. And remove this line.
+
+//1
+const secretNumber = randomInt
+
+function randomInt() {
+    return Math.floor(Math.random()*100)
+}
+
+console.log(secretNumber())
+console.log(secretNumber())
+console.log(secretNumber())
+console.log(secretNumber())
+console.log(secretNumber())
