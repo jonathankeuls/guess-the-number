500593ac02659decf5294e053480440d95047fee
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-02 14:23:19 +0100

    2

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 
 //1
-const secretNumber = randomInt()
+// const secretNumber = randomInt()
 
-function randomInt() {
-    return Math.floor(Math.random()*100)
+// function randomInt() {
+//     return Math.floor(Math.random()*100)
+// }
+
+// console.log(secretNumber)
+// console.log(secretNumber)
+// console.log(secretNumber)
+// console.log(secretNumber)
+// console.log(secretNumber)
+
+
+//2
+function getUserGuess() {  
+    const userInput = document.getElementById('user-input').value
+     return parseInt(userInput, 10)
 }
 
-console.log(secretNumber)
-console.log(secretNumber)
-console.log(secretNumber)
-console.log(secretNumber)
-console.log(secretNumber)
+
+
 
 
