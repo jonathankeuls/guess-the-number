cc19424021d2a041c3340909dc36c847d4e5a561
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-04 14:01:30 +0100

    Solve 4

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 //1
-// const secretNumber = randomInt()
+const secretNumber = randomInt()
 
-// function randomInt() {
-//     return Math.floor(Math.random()*100)
-// }
+function randomInt() {
+    return Math.floor(Math.random()*100)
+}
 
-// console.log(secretNumber)
-// console.log(secretNumber)
-// console.log(secretNumber)
-// console.log(secretNumber)
-// console.log(secretNumber)
+console.log(secretNumber)
+console.log(secretNumber)
+console.log(secretNumber)
+console.log(secretNumber)
+console.log(secretNumber)
 
 
 //2
@@ -20,12 +20,30 @@ function getUserGuess() {
 }
 
 
+
 //3
 document.addEventListener('keyup', function(event){
     if (event.key === 'Enter') {
-        console.log(getUserGuess())
+        const userGuess = getUserGuess()
+        console.log(userGuess)
+        if (userGuess < secretNumber) {
+            setMessage('Too low!')
+        } else if (userGuess > secretNumber) {
+            setMessage('Too big!')
+        } else if (userGuess === secretNumber) {
+            setMessage('You are correct!')
+        }
     }
 })
+
+//4
+function setMessage(msg) {
+    document.getElementById('message').innerText = msg
+}
+
+
+
+
 
 
 
